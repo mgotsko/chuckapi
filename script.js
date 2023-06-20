00d5e8d26dbd1a1eddb553eade9d02dd3f187117
mgotsko <mkostyshyn95@gmail.com>
2023-06-20 18:43:16 -0500

    homework

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 
 const jokesContainer = document.getElementById('jokes-container')
 const newJoke = document.querySelector('#new-joke')
+const newName = document.getElementById('add-name')
+// you will need to get hat id of the imput
 
 function displayOnTheScreen(joke){
     jokesContainer.innerText = joke.value
@@ -8,6 +10,8 @@ function displayOnTheScreen(joke){
 
 async function callToAPI(){
     try{
+        
+        //you need to add the value of the input
     const req =  await fetch('https://api.chucknorris.io/jokes/random')
     const res =  await req.json()
     displayOnTheScreen(res)
@@ -20,3 +24,17 @@ async function callToAPI(){
 newJoke.addEventListener('click',function(){
     callToAPI()
 })
+
+//get elements from dom
+//get the value from dom element
+//add event listener
+//know how function works
+//async await
+//we need to know how to pass data in function
+//use interpolation ``
+
+//if
+//for loop
+//hof
+//const let
+//objects and data types
